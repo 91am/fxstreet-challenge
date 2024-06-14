195cacaf5205e9942bd0c5cd0992456d4d21819b
91am <killaek.info@gmail.com>
2024-06-14 22:48:47 +0200

    refactor layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
 import Card from "./components/card/Card"
 
 function App() {
-  return(
-    <div className="layout">
-      <section className="sidebar">
-        <ul>
-          <li>a</li>
-          <li>s</li>
-          <li>2</li>
-        </ul>
-      </section>
-      <main className="main">
-        <nav className="topbar"></nav>
-        <div className="feed-layout">
-          <section className="news">
-            <div className="filters"></div>
-            <div className="timeline">
+  return (
+    <div class="layout">
+      <div class="sidebar">sidebar</div>
+      <div class="topbar">topbar</div>
+      <div class="filter">filters</div>
+      <main class="main">
+        main
+        <div class="content">
           <Card></Card>
-            </div>
-          </section>
-          <section className="widgets"></section>
-          {/* <h1>papas</h1> */}
         </div>
       </main>
+      <div class="widget">widgets</div>
     </div>
-  )
+  );
 }
 
 export default App
