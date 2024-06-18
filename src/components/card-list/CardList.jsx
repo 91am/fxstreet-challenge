40bc164fa576412de725983a078295d3f6af1692
91am <killaek.info@gmail.com>
2024-06-18 19:40:38 +0200

    add filter logic in card list

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,9 +1,62 @@
 import {useQuery} from "@tanstack/react-query"
 import Card from "../card/Card"
 import { getPosts } from "../../api"
+import { useSearchParams } from "react-router-dom"
 
 export default function CardList() {
     const {data,isLoading,isError} = useQuery({queryKey:"post",queryFn:getPosts})
+    const [searchParams] = useSearchParams()
+
+    const filter = searchParams.get("filter")
+
+    if(filter === "popular") {
+      return (
+        <div>
+          {data
+            ?.filter((post) => post.isPopular === true)
+            .map((post) => (
+              <Card
+                key={post.id}
+                title={post.title}
+                feed={post.feed}
+                subFeed={post.subFeed}
+                author={post.author.name}
+                companyName={post.author.companyName}
+                authorImage={post.author.imageUrl}
+                publicationTime={post.publicationTime}
+                content={post.content}
+              ></Card>
+            ))}
+        </div>
+      );
+    }
+
+    if (filter === "latest") {
+      return (
+        <div>
+          {data
+            ?.sort(
+              (a, b) =>
+                new Date(a.publicationTime) - new Date(b.publicationTime)
+            )
+            .map((post) => (
+              <Card
+                key={post.id}
+                title={post.title}
+                feed={post.feed}
+                subFeed={post.subFeed}
+                author={post.author.name}
+                companyName={post.author.companyName}
+                authorImage={post.author.imageUrl}
+                publicationTime={post.publicationTime}
+                content={post.content}
+              ></Card>
+            ))}
+        </div>
+      );
+    }
+     
+
   return (
     <div>
       {data?.map((post) => (
